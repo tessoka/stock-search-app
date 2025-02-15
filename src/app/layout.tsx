c6d8396e1f785df59bf1e22b78b7e2a0a8cd4316
tessoka <akos.gollitz@gmail.com>
2025-02-15 15:59:34 +0100

    init: create next app

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Stock Search App",
+  description: "Demo for an application of Stock Search",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
